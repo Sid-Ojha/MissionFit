d42817e7ff1989cdec9a0e602c44cbb9083f50f4
Sid-Ojha <90849139+Sid-Ojha@users.noreply.github.com>
2023-07-05 23:50:31 +0530

    Add files via upload

---
+++ fit.js
@@ -0,0 +1,46 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+
+// 
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    effect: "fade",
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+// var swiper = new Swiper(".home-slider", {
+//     spaceBetween: 30,
+//     effect: "fade",
+//     grabCursor:true,
+//         loop:true,
+//        centeredSlides:true,
+    
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+
